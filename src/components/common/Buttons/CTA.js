1ba26dbb3674d2948b43ea433f0cb5bec7b390f6
Matthew Gilligan <mcgilligan97@gmail.com>
2023-02-20 19:37:20 +0900

    Update logo and favicon

--- src/components/common/Buttons/CTA.js
+++ src/components/common/Buttons/CTA.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from './styled';
 
 const CTA = ({ children }) => {
-  return <Container>{children}</Container>;
+  return <Container>{children ? children : 'Come get us'}</Container>;
 };
 
 export default CTA;
